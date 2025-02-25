1d16c3b838b6562742db3afda32f977e3527a93b
A. Jonas Jr <jonasakenande7@gmail.com>
2025-02-25 17:10:48 +0100

    Update serverback.js

--- server/serverback.js
+++ server/serverback.js
@@ -1,9 +1,14 @@
 //Déclaration de constantes & des paramètres de travail
+const dotenv = require('dotenv').config();
 const express = require('express');
-const cors = require();
-const mysql = require();
+const cors = require('cors');
+const mysql = require('mysql');
 const port = 8000; 
-const app= express();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 //Route à empreinter dans le navigateur
 app.get('/', (req, res) => {
@@ -16,40 +21,36 @@ app.listen(port, () => {
 });
 
 //Connexion à la base de données
-
 const bddConnection = mysql.createConnection({
     host: '192.168.64.175',
-    user: 'root',
-    password: 'root',
-    database: 'test'
+    user: 'site1',
+    password: 'yuzu007',
+    database: 'Classement'
 });
 
-bddConnection.connect(
-    function (err) {
-      if (err) throw err; console.log("Vous êtes enfin connecté sur le serveur !");
+bddConnection.connect(function (err) {
+    if (err) throw err;
+    console.log("Vous êtes enfin connecté sur le serveur !");
 });
 
 //Recuperer les données de la table
-
 app.get('/users', (req, res) => {
     bddConnection.query('SELECT * FROM users', function (err, rows) {
         if (err) throw err;
         res.send(rows);
-    })
+    });
 });
 
 //Ajouter des données dans la table 
-
 app.post('/users', (req, res) => {
-    let user = { equipe: 'Equipe 1', classement: 1};
-bddConnection.query('INSERT INTO users SET ?', user, function (err, rows){
-    if (err) throw err;
-    res.send('Utilisateur ajouté à la base de données');
-});
+    let user = { equipe: 'Equipe 1', classement: 1 };
+    bddConnection.query('INSERT INTO users SET ?', user, function (err, rows) {
+        if (err) throw err;
+        res.send('Utilisateur ajouté à la base de données');
+    });
 });
 
 //Modifier des données dans la table
-
 app.put('/users/:id', (req, res) => {
     let id = req.params.id;
     let newClassement = 2;
@@ -58,4 +59,3 @@ app.put('/users/:id', (req, res) => {
         res.send('Classement modifié');
     });
 });
-

a0b99dd1425cc14aac854d81eadc9e0cd35c6d4d
Jonas <site1@gmail.com>
2025-03-05 10:59:36 +0100

    js

--- serverback.js
+++ serverback.js
@@ -3,59 +3,8 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
-const port = 8000; 
+const port = 8001; 
 const app = express();
-const WebSocket = require('ws');
-
-// Connexion à la base de données MySQL
-const db = mysql.createConnection({
-    host: '192.168.64.175',
-    user: 'site1',
-    password: 'yuzu007',
-    database: 'connexion'
-});
-
-db.connect((err) => {
-    if (err) throw err;
-    console.log('Connecté à la base de données MySQL');
-});
-
-// Créez un serveur WebSocket sur le port 8080
-const wss = new WebSocket.Server({ port: 8080 });
-
-wss.on('connection', (ws) => {
-    console.log('Un client est connecté !');
-
-    // Envoyer les messages historiques au client
-    db.query('SELECT * FROM messages ORDER BY created_at DESC LIMIT 10', (err, results) => {
-        if (err) throw err;
-        ws.send(JSON.stringify({ type: 'history', data: results }));
-    });
-
-    // Écouter les messages du client
-    ws.on('message', (message) => {
-        console.log(`Message reçu : ${message}`);
-
-        // Insérer le message dans la base de données
-        db.query('INSERT INTO messages (content) VALUES (?)', [message], (err, results) => {
-            if (err) throw err;
-
-            // Renvoyer le message à tous les clients connectés
-            wss.clients.forEach((client) => {
-                if (client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify({ type: 'message', data: message }));
-                }
-            });
-        });
-    });
-
-    // Gérer la déconnexion
-    ws.on('close', () => {
-        console.log('Le client s\'est déconnecté.');
-    });
-});
-
-console.log('Serveur WebSocket démarré sur ws:192.168.64.175:8080');
 
 // Middleware
 app.use(cors());
@@ -131,19 +80,3 @@ bddConnection.query('SELECT * FROM users', (err, results) => {
   
   // Fermez la connexion (optionnel, selon votre cas d'usage)
   bddConnection.end();
-
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('Classement', 'teddy', 'TE42be98&*', {
-  host: '192.168.64.175',
-  dialect: 'mysql'
-});
-
-// Testez la connexion
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connexion à la base de données réussie !');
-  })
-  .catch((err) => {
-    console.error('Erreur de connexion à la base de données :', err);
-  });

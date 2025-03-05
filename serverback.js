73cabad875bd1c3230b49d2a2c771dc07f44680a
Jonas <site1@gmail.com>
2025-03-05 14:48:54 +0100

    route fonctionnel

--- serverback.js
+++ serverback.js
@@ -3,23 +3,12 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
-const port = 8001; 
+const port = 9100; 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-//Route à empreinter dans le navigateur
-app.get('/', (req, res) => {
-    res.send('Bonjour, ceci est notre serveur (back-end), soyez les bienvenus !');
-});
-
-//Démarrer le serveur
-app.listen(port, () => {
-    console.log(`Le serveur est en écoute sur le port ${port}`);
-});
-
 //Connexion à la base de données
 const bddConnection = mysql.createConnection({
     host: '192.168.64.175',
@@ -32,6 +21,42 @@ bddConnection.connect(function (err) {
     if (err) throw err;
     console.log("Vous êtes enfin connecté sur le serveur !");
 });
+
+
+//Route à empreinter dans le navigateur
+app.get('/', (req, res) => {
+    res.send('Bonjour, ceci est notre serveur (back-end), soyez les bienvenus !');
+});
+
+// route pour la page d'accueil
+app.get('/accueil', (req, res) => {
+    res.send('Vous êtes dans la page d\'accueil. Soyez les bienvenus sur cette page');
+});
+
+// route pour la page de classement
+app.get('/classement', (req, res) => {
+
+    const query = "SELECT m.id, m.Equipe1,  m.Equipe2, m.Gagnant, m.nul,   e1.nom AS nom_equipe1,   e2.nom AS nom_equipe2 FROM  Matchs m JOIN equipe e1 ON m.Equipe1 = e1.id JOIN equipe e2 ON m.Equipe2 = e2.id;";
+
+    bddConnection.query(query, (error, results, fields) => {
+        if (error) throw error;
+      
+        // Traiter les résultats pour calculer le classement
+        const classement = calculerClassement(results);
+      
+        console.log(classement);
+      });
+});
+// route pour la page users
+app.get('/users', (req, res) => {
+    res.send('Vous êtes dans la page users. Soyez les bienvenus sur cette page');
+});
+
+//Démarrer le serveur
+app.listen(port, () => {
+    console.log(`Le serveur est en écoute sur le port ${port}`);
+});
+
 
 //Recuperer les données de la table
 app.get('/users', (req, res) => {
@@ -78,5 +103,41 @@ bddConnection.query('SELECT * FROM users', (err, results) => {
     console.log('Résultats de la requête :', results);
   });
   
-  // Fermez la connexion (optionnel, selon votre cas d'usage)
-  bddConnection.end();
+
+
+  function calculerClassement(matchs) {
+    const classement = {};
+  
+    matchs.forEach(match => {
+      const { Equipe1, Equipe2, Gagnant, nul, nom_equipe1, nom_equipe2 } = match;
+  
+      // Initialiser les équipes dans le classement si elles n'existent pas
+      if (!classement[Equipe1]) {
+        classement[Equipe1] = { id: Equipe1, nom: nom_equipe1, points: 0 };
+      }
+      if (!classement[Equipe2]) {
+        classement[Equipe2] = { id: Equipe2, nom: nom_equipe2, points: 0 };
+      }
+  
+      // Gestion des matchs nuls
+      if (nul === 1) {
+        classement[Equipe1].points += 1;
+        classement[Equipe2].points += 1;
+      } else {
+        // Attribuer les points en fonction du gagnant
+        if (Gagnant === Equipe1) {
+          classement[Equipe1].points += 3;
+        } else if (Gagnant === Equipe2) {
+          classement[Equipe2].points += 3;
+        }
+      }
+    });
+  
+    // Convertir l'objet en tableau pour un classement plus lisible
+    const classementArray = Object.values(classement);
+  
+    // Trier le classement par points décroissants
+    classementArray.sort((a, b) => b.points - a.points);
+  
+    return classementArray;
+  }
